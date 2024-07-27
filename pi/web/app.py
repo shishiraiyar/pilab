ec9d445b89a4505b0d0c1d5474deb736152f114a
Shishira <shishiraiyar@gmail.com>
2024-07-27 18:19:54 +0530

    nginx

--- pi/web/app.py
+++ pi/web/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+visitCount = 0
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return f'Hello, World! This page has been visited {visitCount} times'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
